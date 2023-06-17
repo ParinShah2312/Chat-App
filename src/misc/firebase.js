1cef3870d069a1eccbe260c7f2c09d07cdda1d68
Parin Shah <shahparin2312@gmail.com>
2023-06-17 15:52:30 +0530

    initializaed firebase

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyBGfUVBRJ808kCzw4A0LLshK0kXXfpNmAA',
+  authDomain: 'chat-app-f7851.firebaseapp.com',
+  projectId: 'chat-app-f7851',
+  storageBucket: 'chat-app-f7851.appspot.com',
+  messagingSenderId: '409445021925',
+  appId: '1:409445021925:web:47052b3ff49b756ad3bc0c',
+};
+
+const app = firebase.initializeApp(config);
