3305426536e55071086e6f8de43fe480fc6e1163
Parin Shah <shahparin2312@gmail.com>
2023-06-23 19:03:34 +0530

    displayed chat info

---
+++ src/context/current-room.context.jsx
@@ -0,0 +1,12 @@
+import { createContext, useContextSelector } from 'use-context-selector';
+
+const CurrentRoomContext = createContext();
+
+export const CurrentRoomProvider = ({ children, data }) => {
+  <CurrentRoomContext.Provider value={data}>
+    {children}
+  </CurrentRoomContext.Provider>;
+};
+
+export const useCurrentRoom = selector =>
+  useContextSelector(CurrentRoomContext, selector);
