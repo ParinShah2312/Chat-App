9af00cab97e21d7305da40233a434050235f2b95
Parin Shah <shahparin2312@gmail.com>
2023-06-24 12:22:41 +0530

    created chat bottom send messages in chat

--- src/context/current-room.context.jsx
+++ src/context/current-room.context.jsx
@@ -3,9 +3,11 @@ import { createContext, useContextSelector } from 'use-context-selector';
 const CurrentRoomContext = createContext();
 
 export const CurrentRoomProvider = ({ children, data }) => {
-  <CurrentRoomContext.Provider value={data}>
-    {children}
-  </CurrentRoomContext.Provider>;
+  return (
+    <CurrentRoomContext.Provider value={data}>
+      {children}
+    </CurrentRoomContext.Provider>
+  );
 };
 
 export const useCurrentRoom = selector =>
